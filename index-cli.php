9ee3aca2a2e67fe8a581cd5d36712f558fa938dd
valeriominetti <valerio.minetti@gmail.com>
2014-05-19 13:08:26 +0000

    removed comments, code cleaning

--- index-cli.php
+++ index-cli.php
@@ -1,13 +1,13 @@
 <?php
 require_once("./src/snapchat.php");
 // Log in:
-$snapchat = new Snapchat('snaps_roulette', 'snaproulette');
+$snapchat = new Snapchat('username', 'password');
 $snaps = $snapchat->getSnaps();
 $files = glob('./archive'. '/*.*');
 $ext=".jpeg";
 $type=Snapchat::MEDIA_IMAGE;
 
-//echo "<table>";
+// Cycle through received snaps
 foreach ($snaps as $snap)
 {
 		// get snap file and save it
@@ -19,20 +19,17 @@ foreach ($snaps as $snap)
                 if ($head[0] == chr(0xFF) && $head[1] == chr(0xD8)) {
 			$ext=".jpg";
                 }
-
                 // Check for a MP4 header.
                 if ($head[0] == chr(0x00) && $head[1] == chr(0x00)) {
                         echo "mp4";
 			$ext=".mp4";
                 }
 
-
 		$filename='./archive/'.$snap->sender.'-'.$snap->id.$ext;
 		file_put_contents($filename, $data);
 		$snapchat->markSnapViewed($snap->id); 
-		//echo "<tr><td>".$snap->sender.'</td>'.'<td><img src='.$filename.' ></td>'.'</tr>';
 
-		// send back random file
+		// get a random file from archive
 		$file = array_rand($files);
 
 		// find out if selected file is mp4 or jpg
@@ -44,15 +41,16 @@ foreach ($snaps as $snap)
                         echo "mp4";
  			$type=Snapchat::MEDIA_VIDEO;
 		}
+		// Snap back to user
 		$id = $snapchat->upload($type,file_get_contents($files[$file]));
 		$snapchat->send($id, array($snap->sender), 8);
 }
-//echo "</table>";
 
+// Clear bot feed
 $snapchat->clearFeed();
 
 
-// send random snap to story
+// send random snap to story every 30 mins
 $mins=date('i');
 if ($mins == '30' || $mins == '00'){
 
@@ -61,6 +59,7 @@ if ($mins == '30' || $mins == '00'){
                 if (filesize($file) == '0' ) { unlink ($file); }
         }
 
+	// select a rando file from archive
         $file = array_rand($files);
         // find out if selected file is mp4 or jpg
         $res=strpos($files[$file],".jpg");
@@ -71,6 +70,7 @@ if ($mins == '30' || $mins == '00'){
                echo "mp4";
                $type=Snapchat::MEDIA_VIDEO;
         }
+	// Send snap to bot story
         $id = $snapchat->upload($type,file_get_contents($files[$file]));
         $snapchat->setStory($id, $type);
 }
